a0683e0a524952d5fb921da11e11b58f1293324e
maythamghaly <maythamghaly@gmail.com>
2024-11-14 00:33:10 +0200

    add tailwind css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL, // Access the environment variable here
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css",
+    '~/assets/global.css',
+    '@fortawesome/fontawesome-free/css/all.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@pinia/nuxt', // This will automatically set up Pinia for you
+  ],
 })
